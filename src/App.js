70073105f67b38a7a97aac5940adf15f5c5a2de5
harizikhalil <khalilharizi3@gmail.com>
2020-11-30 20:05:55 +0100

    finished redux counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux'
+import {Inc,Dec} from './js/action'
 
-function App() {
+function App({counter,inrement,decrement}) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={ counter >0 ? decrement:undefined}>-</button>
+     <span>{counter}</span>
+     <button onClick={ inrement}>+</button>
     </div>
   );
 }
-
-export default App;
+const mapStateToProps=(state)=>{
+  return{
+    counter:state.count
+  }
+}
+const mapDispatchToProps=(dispatch)=>{
+  return{
+    inrement:() => dispatch(Inc()),
+    decrement:() => dispatch(Dec())
+  }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(App);
---
+++ src/js/action/index.js
@@ -0,0 +1,13 @@
+import {INCREMENT,DECREMENT} from "../actionTypes";
+
+export const Inc=()=>{
+    return{
+        type:INCREMENT
+    }
+}
+
+export const Dec=()=>{
+    return{
+        type:DECREMENT
+    }
+}
